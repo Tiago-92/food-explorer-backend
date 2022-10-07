051b8ef51d998530124b5f79916aed597c2b0ca7
Tiago <tiagoamaralcbei@hotmail.com>
2022-10-07 09:49:31 -0300

    created migrate for dishs and ingredients

---
+++ src/database/knex/migrations/20221007115556_createDishs.js
@@ -0,0 +1,14 @@
+exports.up = knex => knex.schema.createTable("dishs", table=> {
+   table.increments("id");
+   table.text("name");
+   table.text("description");
+   table.integer("price");
+   table.text("img");
+   table.integer("user_id").references("id").inTable("users");
+
+   table.timestamp("created_at").default(knex.fn.now());
+   table.timestamp("update_at").default(knex.fn.now());
+
+});
+
+exports.down = knex => knex.schema.dropTable("dishs");
